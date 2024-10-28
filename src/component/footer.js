08bce0c0ca6faf2e102c5d15333843c12528d5dc
AhWeiHENGuEi <aa9440307@gmail.com>
2024-10-28 15:36:40 +0800

    feat: 更新 ESLint 配置，關閉 Next.js 圖像元素警告；調整 GitHub Actions 工作流以支持多個分支並使用 pnpm；修正頁面內容

--- src/component/footer.js
+++ src/component/footer.js
@@ -81,7 +81,7 @@ const footer = () => {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2022{'{'}' '{'}'}
+            © 2022
             <a href="https://flowbite.com" className="hover:underline">
               Flowbite™
             </a>
